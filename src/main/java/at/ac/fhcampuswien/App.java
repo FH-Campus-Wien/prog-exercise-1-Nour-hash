64ef98d85d13a39d9990adb0389600826f48dc7a
Nour-hash <113339425+Nour-hash@users.noreply.github.com>
2022-09-29 12:09:02 +0200

    final version finished

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -32,14 +32,26 @@ public class App {
     //todo Task 3
     public void sumOfLiterals(){
         // input your solution here
-/*
+
+
         char inter1 ='Z';
         int inter2 =0xface;
         int inter3 = 012;
+        long lon1 = 80L;
+        float fl= 44e-1f;
+        float fl1 = 5.5f;
+        double db = 8.88e1;
+        double db1= 99.9;
 
-   80L, 44e-1f, 5.5f, 8.88e1 und 99.9
+        int number = inter1;
+        int number1 = (int)db;
+        int number2 = (int)db1;
+        int number3 = (int)fl;
+        int number4 = (int)fl1;
+        int number5 = Math.toIntExact(lon1);
+        int sum = number+inter2+inter3+number5+number3+number4+number1+number2;
+        System.out.println(sum);
 
-   */
     }
 
     //todo Task 4
@@ -104,28 +116,89 @@ public class App {
         System.out.print("Enter annual Revenue: ");
         int rev = input.nextInt();
 
-        for(int i = 0 ; i>3; i++)
-        {
-            System.out.println(rev);
+        if (rev < 0 || rev >= 100000){
+            System.out.println( "Invalid Revenue");
         }
+        else if (0 <= rev && rev< 20000) {
+            System.out.println( "Poor Sales Revenue");
+        }
+        else if (20000 <= rev && rev< 50000) {
+            System.out.println( "Average Sales Revenue");
+        }
+        else if (50000 <= rev && rev< 80000) {
+            System.out.println( "Good Sales Revenue");
+        }
+        else if (80000 <= rev && rev< 100000) {
+            System.out.println( "Excellent Sales Revenue" );
+        }
+
 
     }
 
     //todo Task 8
-    public void getCommissionRate(){
+    public void getCommissionRate() {
         // input your solution here
-    }
 
+        Scanner input1 = new Scanner(System.in);
+        System.out.print("Enter CommissionClass: ");
+        int pro = input1.nextInt();
+
+        switch (pro) {
+            case 1:
+                System.out.println("Your Commission Rate was set to 0.01");
+                break;
+            case 2:
+                System.out.println("Your Commission Rate was set to 0.02");
+                break;
+            case 3:
+                System.out.println("Your Commission Rate was set to 0.03");
+                break;
+            case 4:
+                System.out.println("Your Commission Rate was set to 0.04");
+                break;
+            default:
+                System.out.println("Your Commission Rate was set to 0.0");
+                break;
+
+        }
+    }
     //todo Task 9
-    public void leapyear(){
+    public void leapyear() {
         // input your solution here
-    }
 
+        System.out.print("Year: ");
+        Scanner input2 = new Scanner(System.in);
+        int year = input2.nextInt();
+
+        if (year % 4 == 0 && year % 400 == 0) {
+
+            System.out.println("Leapyear");
+
+        }
+        else if (year % 100 == 0) {
+
+            System.out.println("Not a Leapyear");
+        }
+        else {
+            System.out.println("Not a Leapyear");
+        }
+
+
+    }
     //todo Task 10
     public void transposedNumbers(){
         // input your solution here
+        int rever=0;
+        Scanner input1= new Scanner(System.in);
+        int number = input1.nextInt();
+        System.out.print("Number: ");
+        for( ;number != 0; number=number/10)
+        {
+            int uebrig = number % 10;
+            rever = rever * 10 + uebrig;
+        }
+        System.out.println(rever);
     }
-
 
     public static void main(String[] args){
         App exercise1 = new App();
